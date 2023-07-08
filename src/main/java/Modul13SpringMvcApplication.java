5ea2a26d0f98fea42beba198b40b6b07b90e01d5
OpaChaUA <opaCha.ua.94@gmail.com>
2023-07-08 11:10:30 +0300

    Initial commit

---
+++ src/main/java/Modul13SpringMvcApplication.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Modul13SpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Modul13SpringMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Modul13SpringMVC/Modul13SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Modul13SpringMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Modul13SpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
